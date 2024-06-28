fae007c9255cb17f1d4069e23c25a1ef0fbc9ea0
Wuzak <wuzak7@gmail.com>
2024-06-28 12:16:31 +0300

    remove db connection

--- models.py
+++ models.py
@@ -1,7 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from database import Base
 
 
 class Tasks(Base):
